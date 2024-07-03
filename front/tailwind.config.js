bc1d4789b0cb4d5b67cbc0602c6687985a9366ec
AlexMel83 <meleshko.a.n@ukr.net>
2024-07-03 19:00:47 +0300

    add structure

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,7 +1,7 @@
-const animate = require("tailwindcss-animate")
+import animate from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   darkMode: ["class"],
   safelist: ["dark"],
   prefix: "",
@@ -107,3 +107,5 @@ module.exports = {
   },
   plugins: [animate],
 }
+
+export default config;
