c8a3c05314f72d495ef0cb9fe20a298a581a50d6
AlexMel83 <meleshko.a.n@ukr.net>
2024-07-22 00:20:39 +0300

    addCard-155

---
+++ front/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="realative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 transition hover:shadow-xl"
+  >
+    <img
+      src="../public/like-2.svg"
+      alt="like-2"
+      class="absolute top-8 left-8"
+    />
+    <img src="../public/sneakers/sneakers-1.jpg" alt="sneakers" />
+    <p class="mt-2">Man's shoes Nike Blazer Mid Suede</p>
+    <div class="flex items-center justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Price:</span>
+        <b>1205 uah</b>
+      </div>
+      <img src="../public/plus.svg" alt="plus" />
+    </div>
+  </div>
+</template>
---
+++ front/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header
+    class="flex items-center justify-between border-b border-slate-300 px-10 py-8"
+  >
+    <div class="flex items-center gap-4">
+      <img class="w-10" src="./../public/logo.png" alt="Logo" />
+      <div>
+        <h2 class="font-bold text-xl uppercase">Vue Sneakers</h2>
+        <p class="text-slate-500">The Shop of the best shoes</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10 text-gray-500">
+      <li class="flex items-center gap-3 hover:text-black cursor-pointer">
+        <img src="./../public/cart.svg" alt="Cart" />
+        <b>1205 uah</b>
+      </li>
+      <li class="flex items-center gap-3 hover:text-black cursor-pointer">
+        <img src="./../public/heart.svg" alt="Cart" />
+        <span>Favorites</span>
+      </li>
+      <li class="flex items-center gap-3 hover:text-black cursor-pointer">
+        <img src="./../public/profile.svg" alt="Cart" />
+        <span>Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
