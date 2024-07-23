98b515557a983bdfa619e541d13652fa93317789
AlexMel83 <meleshko.a.n@ukr.net>
2024-07-23 12:10:16 +0300

    332

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -9,11 +9,12 @@
         <h2 class="text-3xl font-bold mb-8">All Shoes</h2>
         <div class="flex gap-4 items-center">
           <select
+            @change="onChangeSelect"
             class="py-2 px-3 bordrer rounded-md outline-none items-center"
           >
-            <option>By Name</option>
-            <option>By Low Price</option>
-            <option>By High Price</option>
+            <option value="title">By Name</option>
+            <option value="price">By Low Price</option>
+            <option value="-price">By High Price</option>
           </select>
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
@@ -45,10 +46,29 @@ interface Item {
 }
 
 const items = ref<Item[]>([]);
+const sortBy = ref("");
+const searchQuery = ref("");
+
+const onChangeSelect = (event: Event) => {
+  if (event.target) {
+    sortBy.value = event.target.value;
+  }
+};
 
 onMounted(async () => {
   try {
     const { data } = await axios.get("http://localhost:4041/sneakers");
+    items.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+watch(sortBy, async () => {
+  try {
+    const { data } = await axios.get(
+      "http://localhost:4041/sneakers?sortBy=" + sortBy.value,
+    );
     items.value = data;
   } catch (error) {
     console.error(error);
